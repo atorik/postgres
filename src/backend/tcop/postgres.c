1df46b95a26d85835fb05b13b01dd502f8fc24cd
Atsushi <torikoshia@oss.nttdata.com>
2022-02-03 18:26:16 +0900

    Add function to log the plan of the query      currently running on the backend.
        Currently, we have to wait for the query execution to finish
        to check its plan. This is not so convenient when
        investigating long-running queries on production environments
        where we cannot use debuggers.
        To improve this situation, this patch adds
        pg_log_query_plan() function that requests to log the
        plan of the specified backend process.
    
        By default, only superusers are allowed to request to log the
        plans because allowing any users to issue this request at an
        unbounded rate would cause lots of log messages and which can
        lead to denial of service.
    
        On receipt of the request, at the next CHECK_FOR_INTERRUPTS(),
        the target backend logs its plan at LOG_SERVER_ONLY level, so
        that these plans will appear in the server log but not be sent
        to the client.
    
        Reviewed-by: Bharath Rupireddy, Fujii Masao, Dilip Kumar,
        Masahiro Ikeda, Ekaterina Sokolova, Justin Pryzby, Kyotaro
        Horiguchi, Robert Treat

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -41,6 +41,7 @@
 #include "access/xact.h"
 #include "catalog/pg_type.h"
 #include "commands/async.h"
+#include "commands/explain.h"
 #include "commands/prepare.h"
 #include "common/pg_prng.h"
 #include "executor/spi.h"
@@ -3367,6 +3368,9 @@ ProcessInterrupts(void)
 
 	if (LogMemoryContextPending)
 		ProcessLogMemoryContextInterrupt();
+
+	if (LogQueryPlanPending)
+		ProcessLogQueryPlanInterrupt();
 }
 
 
