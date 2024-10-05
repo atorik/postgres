ad96eb1ef07c8dc5bcc0dfe8df76d29d1cf385fa
Atsushi Torikoshi <torikoshia@oss.nttdata.com>
2024-10-05 13:33:25 +0900

    Refactored

--- src/backend/commands/copyfrom.c
+++ src/backend/commands/copyfrom.c
@@ -1018,7 +1018,7 @@ CopyFrom(CopyFromState cstate)
 			pgstat_progress_update_param(PROGRESS_COPY_TUPLES_SKIPPED,
 										 cstate->num_errors);
 
-			if (cstate->opts.reject_limit && skipped > cstate->opts.reject_limit)
+			if (cstate->opts.reject_limit && cstate->num_errors > cstate->opts.reject_limit)
 				ereport(ERROR,
 						(errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
 						 errmsg("skipped more than REJECT_LIMIT rows: \"%lld\",",
