1a54b894caea5f7e39bd25610a93b60b72dcf23d
Atsushi Torikoshi <torikoshia@oss.nttdata.com>
2026-06-15 22:17:33 +0900

    pg_indented

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -5380,10 +5380,9 @@ AbortSubTransaction(void)
 	ResetLogicalStreamingState();
 
 	/*
-	 * Reset current query plan state.
-	 * Note that even after this reset, it's still possible to obtain
-	 * the parent transaction's query plans, since they are preserved
-	 * in standard_ExecutorRun().
+	 * Reset current query plan state. Note that even after this reset, it's
+	 * still possible to obtain the parent transaction's query plans, since
+	 * they are preserved in standard_ExecutorRun().
 	 */
 	SetCurrentQueryDesc(NULL);
 
